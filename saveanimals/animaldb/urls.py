bba5f67703f2caeaaff8b6a3eecda56ced06445a
rajdeepbharati <rajdeepbharati13@gmail.com>
2018-09-22 15:29:59 +0530

    updated template

--- saveanimals/animaldb/urls.py
+++ saveanimals/animaldb/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from .views import animal_list, animal_detail
 
 app_name = "animaldb"
 urlpatterns = [
 
     path('', animal_list, name="animal_list"),
-    path(r'^(?P<slug>[\w-]+)/$',animal_detail, name="animal_detail")
+    re_path(r'^(?P<slug>[\w-]+)/$',animal_detail, name="animal_detail")
     # re_path(r'^(?P<slug>[\w-]+)/$', post_detail, name='detail'),
 ]
