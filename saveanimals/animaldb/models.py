8d7046587ad8b8e760ada0313654d450efa59018
rajdeepbharati <rajdeepbharati13@gmail.com>
2018-09-22 23:14:52 +0530

    added models

--- saveanimals/animaldb/models.py
+++ saveanimals/animaldb/models.py
@@ -2,10 +2,27 @@ from django.db import models
 from django.urls import reverse
 # Create your models here.
 
+
+def upload_location(instance, filename):
+    """
+    instance.__class__ gets the model Post. We must use this method because the model is defined below.
+    Then create a queryset ordered by the "id"s of each object,
+    Then we get the last object in the queryset with `.last()`
+    Which will give us the most recently created Model instance
+    We add 1 to it, so we get what should be the same id as the the post we are creating.
+    """
+    return "%s/%s" %(instance.id, filename)
+
 class AnimalProduct(models.Model):
     name = models.CharField(max_length=25, null=False)
     details = models.TextField()
-
+    image = models.ImageField(upload_to=upload_location,
+            null=True,
+            blank=True,
+            width_field="width_field",
+            height_field="height_field")
+    height_field = models.IntegerField(default=400)
+    width_field = models.IntegerField(default=500)
     def __str__(self):
         return self.name
 
@@ -14,7 +31,14 @@ class AnimalDb(models.Model):
     location = models.CharField(max_length=10, null=False)
     slug = models.SlugField()
     animal_product = models.ManyToManyField(AnimalProduct)
-
+    image = models.ImageField(upload_to=upload_location,
+            null=True,
+            blank=True,
+            width_field="width_field",
+            height_field="height_field")
+    height_field = models.IntegerField(default=400)
+    width_field = models.IntegerField(default=500)
+    
     def __str__(self):
         return self.name
 
--- saveanimals/animaldb/urls.py
+++ saveanimals/animaldb/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
 
     path('', animal_list, name="animal_list"),
     re_path(r'^(?P<slug>[\w-]+)/$',animal_detail, name="animal_detail")
-    # re_path(r'^(?P<slug>[\w-]+)/$', post_detail, name='detail'),
+
 ]
